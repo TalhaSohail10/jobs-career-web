08f7421505ae72dbc64847c55ac235a2995a50ae
Muhammad Talha <106464786+TalhaSohail10@users.noreply.github.com>
2025-04-30 13:00:59 +0000

    Adding Html "Home" page and Styling with css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)  
 
 
 @app.route('/') 
 def home():
-  return "Hello, Flask! first commit"
+  return render_template('home.html')
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
